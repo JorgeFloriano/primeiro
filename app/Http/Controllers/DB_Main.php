864805de3feb85da32e88411a6fab7fab3420588
Jorge Luis <jorgefloriano1646@gmail.com>
2024-02-18 11:16:33 -0300

    DB2

--- app/Http/Controllers/DB_Main.php
+++ app/Http/Controllers/DB_Main.php
@@ -4,19 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class DB_Main extends Controller
 {
     public function home() {
         echo '<h1>DATABASES</h1>';
-        $users = DB::select("SELECT * FROM users");
-        //dd($users);
+        // $users = DB::select("SELECT * FROM users");
+        // dd($users);
 
         // echo '<pre>';
         // print_r($users);
 
-        foreach ($users as $user) {
-            echo '<p>'.$user->id.' '.$user->name.'</p>';
-        }
+        // foreach ($users as $user) {
+        //     echo '<p>'.$user->id.' '.$user->name.'</p>';
+        //}
+        
+        // $parameters = [
+        //     'Ana'
+        // ];
+        // $users = DB::select("SELECT * FROM users WHERE name = ?", $parameters);
+        // dd($users);
+
+        // $parameters = [
+        //     ':n' => 'Ana'
+        // ];
+        // $users = DB::select("SELECT * FROM users WHERE name = :n", $parameters);
+        // dd($users);
+
+        $user = new User();
+        $results = $user->getUser('João');
+        dd($results);
     }
 }
